27242f2d65bdf4acd39c72e854c2eb18f6c4c419
Nicolás S. Quiroz <nicosquiroz@gmail.com>
2026-06-14 23:45:24 -0400

    Set production base URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'app/learning/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://www.appliednewworlds.com'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
